6f1c2ad8eed84ddf3d1b49c291c2b056d76fe002
lyraeanggarda <40013819+lyraeanggarda@users.noreply.github.com>
2018-07-05 09:16:52 +0700

    Update UnblockMe.java

--- UnblockMe.java
+++ UnblockMe.java
@@ -11,6 +11,10 @@ package unblockme;
  */
 public class UnblockMe {
 
+    private static int nSimpul = 0;
+    private Block[] map;
+    private static ArrayList<Step> steps = new ArrayList<Step>();
+
     /**
      * @param args the command line arguments
      */
@@ -20,9 +24,20 @@ public class UnblockMe {
     
 }
 public class block {
-    private int idx;
+    private int idx; // idx = 0 untuk block utama
     private int length;
     private int x;
     private int y;
-    private char jenis;
+    private char jenis; // vertikal (v) atau horizontal (h)
+}
+class simpul {
+    private int idx;
+    private ArrayList<Integer> nodes;
+    private boolean isSolusi;
+}
+class step {
+    public int idBlock;
+    public char direction; // w/a/s/d
+    public int weight;
+    public int idSimpul;
 }
